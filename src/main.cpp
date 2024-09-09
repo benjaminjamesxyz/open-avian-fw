c80b5443ce7e86791f434bc36ab4fde80e2748f7
Krishna Bhat <memotoskbhat@gmail.com>
2024-09-09 20:19:05 +0530

    Added program to make use of RTOS Tasks

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,27 @@
 #include <FreeRTOS.h>
 #include <FreeRTOSConfig.h>
+#include <pico/cyw43_arch.h>
+#include <pico/stdio.h>
+#include <pico/stdlib.h>
 #include <task.h>
 
 #include <main.h>
 
-int main() {
+void led_status(void *pvParameters) {
+  stdio_init_all();
   while (1) {
-
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+    sleep_ms(250);
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+    sleep_ms(250);
   }
+}
+
+int main() {
+  stdio_init_all();
+
+  xTaskCreate(led_status, "LED_STATUS", 256, NULL, 1, NULL);
+  vTaskStartScheduler();
   return 0;
 }
+
